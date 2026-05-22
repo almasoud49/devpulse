8ec240f67e706e49c3e302a3de41b60a0373bc29
Abdullah Al Masoud <almasoud49@yahoo.com>
2026-05-22 08:58:45 +0600

    [DONE] User Login Credentials Validation

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,26 @@
+import bcrypt from "bcryptjs";
+import { pool } from "../../db";
 
-const lonignUserIntoDB = async(payload: any)=>{
+const lonignUserIntoDB = async(payload: {
+        email: string, 
+        password: string})=>{
+    const {email ,password} = payload;
+
+    const result = await pool.query(`
+        SELECT * FROM "users" WHERE "email"=$1
+        `,[email]);
+    const user = result.rows[0];
+    // console.log(user)
+
+    if(user.rows?.length === 0){
+        throw new Error("Invalid Credentials! User not Found.")
+    }
+
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if(!matchPassword){
+        throw new Error("Not Matched! Invalid Credentials!")
+    }
 
 }
 
